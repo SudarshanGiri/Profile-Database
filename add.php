966b4a940d3db59c059d18232e8952b389a0b8f8
Sudarshan Giri <41857483+SudarshanGiri@users.noreply.github.com>
2020-07-02 20:57:01 +0545

    added education section

--- add.php
+++ add.php
@@ -50,29 +50,67 @@
             ':he' => $_POST['headline'],
             ':su' => $_POST['summary'])
         );
+
+
         $profile_id = $pdo->lastInsertId();
         $rank=1;
         for($i=1; $i<=9; $i++) {
-                $year = $_POST['year'.$i];
-                $desc = $_POST['desc'.$i];
-                if($year!==null){
-                    $stmt = $pdo->prepare('INSERT INTO Position
-                    (profile_id, rank, year, description) 
-                    VALUES ( :pid, :rank, :year, :desc)');
-                    $stmt->execute(array(
-                        ':pid' => $profile_id,
-                        ':rank' => $rank,
-                        ':year' => $year,
-                        ':desc' => $desc)
-                    );
-                    $rank++;
+            if(!isset($_POST['year'.$i])) continue;
+            if(!isset($_POST['desc'.$i])) continue;
+
+            $year = $_POST['year'.$i];
+            $desc = $_POST['desc'.$i];
+            if($year!==null){
+                $stmt = $pdo->prepare('INSERT INTO Position
+                (profile_id, rank, year, description) 
+                VALUES ( :pid, :rank, :year, :desc)');
+                $stmt->execute(array(
+                    ':pid' => $profile_id,
+                    ':rank' => $rank,
+                    ':year' => $year,
+                    ':desc' => $desc)
+                );
+                $rank++;
+            }             
+        }
+
+        $rank=1;
+        for($i=1; $i<=9; $i++) {
+            if(!isset($_POST['edu_year'.$i])) continue;
+            if(!isset($_POST['edu_school'.$i])) continue;
+
+            $year = $_POST['edu_year'.$i];
+            $school = $_POST['edu_school'.$i];
+
+            //Lookup the school if it is there
+            $institution_id=false;
+            $stmt=$pdo->prepare('SELECT institution_id FROM Institution WHERE name=:name');
+            $stmt->execute(array(':name'=>$school));
+            $row=$stmt->fetch(PDO::FETCH_ASSOC);
+            if ($row!=false) $institution_id=$row['institution_id'];
+
+            //if there was no institution, insert it
+            if($institution_id==false){
+                $stmt=$pdo->prepare('INSERT INTO Institution (name) VALUES (:name)');
+                $stmt->execute(array(':name' => $school));
+                $institution_id=$pdo->lastInsertId();
+            }
+
+                $stmt = $pdo->prepare('INSERT INTO education
+                (profile_id, rank, year, institution_id) 
+                VALUES ( :pid, :rank, :year, :iid)');
+                $stmt->execute(array(
+                    ':pid' => $profile_id,
+                    ':rank' => $rank,
+                    ':year' => $year,
+                    ':iid' => $institution_id)
+                );
+                $rank++;
+            }             
     
 
 
 
-                }
-               
-        }
         $_SESSION['message'] = "Profile added";
 
         header('Location: index.php');
@@ -90,37 +128,51 @@
     <title>Sudarshan Giri</title>
 </head>
 <body>
-<h1>Adding Profile for <?php echo $_SESSION['name']; ?></h1>
-<form method="post">
-<p>First Name:
-<input type="text" id="fname" name="first_name" size="60"/></p>
-<p>Last Name:
-<input type="text" id="lname" name="last_name" size="60"/></p>
-<p>Email:
-<input type="text" id="email" name="email" size="30"/></p>
-<p>Headline:<br/>
-<input type="text" id="headline" name="headline" size="80"/></p>
-<p>Summary:<br/>
-<textarea id="summary" name="summary" rows="8" cols="80"></textarea>
+    <div class="container">
+        <h1>Adding Profile for <?php echo $_SESSION['name']; ?></h1>
+        <form method="post">
+        <p>First Name:
+        <input type="text" id="fname" name="first_name" size="60"/></p>
+        <p>Last Name:
+        <input type="text" id="lname" name="last_name" size="60"/></p>
+        <p>Email:
+        <input type="text" id="email" name="email" size="30"/></p>
+        <p>Headline:<br/>
+        <input type="text" id="headline" name="headline" size="80"/></p>
+        <p>Summary:<br/>
+        <textarea id="summary" name="summary" rows="8" cols="80"></textarea>
+
+        <p>
+        Education: <input type="submit" id="addEdu" value="+">
+        <div id="edu_fields">
+        </div>
+        </p>
+
+        <p>
+        Position: <input type="submit" id="addPos" value="+">
+        <div id="position_fields">
+        </div>
+        </p>
 
 
-<p>
-Position: <input type="submit" id="addPos" value="+">
-<div id="position_fields">
-</div>
-</p>
 
-<p>
-<input type="submit"  value="Add" name="add" onclick="validate();">
-<input type="submit" name="cancel" value="Cancel">
-</p>
+        <p>
+        <input type="submit"  value="Add" name="add" onclick="validate();">
+        <input type="submit" name="cancel" value="Cancel">
+        </p>
 
-</form>
+        </form>
+    </div>
+
+
+
+
+
+
+
+
+
     
-</body>
-</html>
-
-
 
 <script>
 function validate(){
@@ -150,6 +202,7 @@ function validate(){
 
 
 //validate position------------------------------------
+countEdu=0;
 countPos=0;
 $(document).ready(function(){
     window.console && console.log('Document ready called');
@@ -174,6 +227,35 @@ $(document).ready(function(){
 
 
 
+    $('#addEdu').click(function(event){
+        event.preventDefault();
+        if ( countEdu >= 9 ) {
+            alert("Maximum of nine education entries exceeded");
+            return;
+        }
+        countEdu++;
+        window.console && console.log("Adding education "+countEdu);
+
+        $('#edu_fields').append(
+            '<div id="edu'+countEdu+'"> \
+            <p>Year: <input type="text" name="edu_year'+countEdu+'" value="" /> \
+            <input type="button" value="-" onclick="$(\'#edu'+countEdu+'\').remove();return false;"><br>\
+            <p>School: <input type="text" size="80" name="edu_school'+countEdu+'" class="school" value="" />\
+            </p></div>'
+        );
+
+        $('.school').autocomplete({
+            source: "school.php"
+        });
+
+    });
+
 
 });
+
+
 </script>
+   
+</body>
+</html>
+
