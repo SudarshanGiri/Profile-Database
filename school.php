ac31064aa9f6f238d95f3de77e8bd2c10dfe01f3
Sudarshan Giri <41857483+SudarshanGiri@users.noreply.github.com>
2020-07-02 20:58:14 +0545

    added fetching of institution and return them as json

---
+++ school.php
@@ -0,0 +1,9 @@
+<?php
+include "dbcon.php";
+$stmt = $pdo->prepare('SELECT name FROM Institution WHERE name LIKE :prefix');
+$stmt->execute(array( ':prefix' => $_REQUEST['term']."%"));
+$retval = array();
+while ( $row = $stmt->fetch(PDO::FETCH_ASSOC) ) {
+  $retval[] = $row['name'];
+}
+echo(json_encode($retval, JSON_PRETTY_PRINT));
